3e8b4ea1123cc255018d4c9c3cd39b94cc4ef712
danielder10 <dder10@my.bcit.ca>
2024-12-09 15:21:03 -0800

    fixing issues with jsx

--- app/HomePage/page.tsx
+++ app/HomePage/page.tsx
@@ -12,10 +12,24 @@ const Home: React.FC = () => {
         <button className="mt-4 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600">
           Explore More
         </button>
-        <div className="flex justify-center space-x-4 mt-6">
-          <img src="/lantern1.png" alt="Lantern 1" className="w-10 h-10" />
-          <img src="/lantern2.png" alt="Lantern 2" className="w-10 h-10" />
-          <img src="/lantern3.png" alt="Lantern 3" className="w-10 h-10" />
+
+        {/* Lanterns */}
+        <div className="flex justify-center space-x-8 mt-6">
+          <div className="relative w-16 h-24 bg-red-300 rounded-md shadow-md">
+            <div className="absolute top-0 w-6 h-6 bg-red-500 rounded-full left-1/2 transform -translate-x-1/2"></div>
+            <div className="absolute bottom-0 w-4 h-4 bg-red-500 rounded-full left-1/2 transform -translate-x-1/2"></div>
+            <div className="absolute w-2 h-8 bg-red-500 top-1/4 left-1/2 transform -translate-x-1/2"></div>
+          </div>
+          <div className="relative w-16 h-24 bg-blue-300 rounded-md shadow-md">
+            <div className="absolute top-0 w-6 h-6 bg-blue-500 rounded-full left-1/2 transform -translate-x-1/2"></div>
+            <div className="absolute bottom-0 w-4 h-4 bg-blue-500 rounded-full left-1/2 transform -translate-x-1/2"></div>
+            <div className="absolute w-2 h-8 bg-blue-500 top-1/4 left-1/2 transform -translate-x-1/2"></div>
+          </div>
+          <div className="relative w-16 h-24 bg-pink-300 rounded-md shadow-md">
+            <div className="absolute top-0 w-6 h-6 bg-pink-500 rounded-full left-1/2 transform -translate-x-1/2"></div>
+            <div className="absolute bottom-0 w-4 h-4 bg-pink-500 rounded-full left-1/2 transform -translate-x-1/2"></div>
+            <div className="absolute w-2 h-8 bg-pink-500 top-1/4 left-1/2 transform -translate-x-1/2"></div>
+          </div>
         </div>
       </header>
 
@@ -23,9 +37,15 @@ const Home: React.FC = () => {
       <section className="py-10 bg-white text-center">
         <h2 className="text-2xl font-bold text-gray-800">Explore Festivals</h2>
         <div className="flex justify-center space-x-6 mt-4">
-          <div className="bg-red-100 p-6 rounded-full shadow-lg">🎆</div>
-          <div className="bg-blue-100 p-6 rounded-full shadow-lg">🎋</div>
-          <div className="bg-pink-100 p-6 rounded-full shadow-lg">🌸</div>
+          <div className="w-20 h-20 bg-red-200 rounded-full flex items-center justify-center shadow-lg text-red-500 text-3xl">
+            🎆
+          </div>
+          <div className="w-20 h-20 bg-blue-200 rounded-full flex items-center justify-center shadow-lg text-blue-500 text-3xl">
+            🎋
+          </div>
+          <div className="w-20 h-20 bg-pink-200 rounded-full flex items-center justify-center shadow-lg text-pink-500 text-3xl">
+            🌸
+          </div>
         </div>
       </section>
 
@@ -61,11 +81,15 @@ const Home: React.FC = () => {
 
       {/* Footer */}
       <footer className="bg-white py-10 text-center">
-        <img
-          src="/torii.png"
-          alt="Torii Gate"
-          className="w-24 mx-auto"
-        />
+        {/* Torii Gate */}
+        <div className="relative w-40 h-20 mx-auto">
+          <div className="absolute top-0 left-0 w-full h-4 bg-red-600"></div>
+          <div className="absolute top-4 left-2 w-2 h-16 bg-red-600"></div>
+          <div className="absolute top-4 right-2 w-2 h-16 bg-red-600"></div>
+          <div className="absolute top-8 left-6 w-28 h-2 bg-red-500"></div>
+          <div className="absolute bottom-0 left-2 w-8 h-2 bg-red-500"></div>
+          <div className="absolute bottom-0 right-2 w-8 h-2 bg-red-500"></div>
+        </div>
       </footer>
     </div>
   );
